3c88b58e4f7dac413e14b6db200793026c32a2bb
Joseph Tawil <jtawil@mail.sfsu.edu>
2020-01-13 00:20:31 -0800

    Adding files

---
+++ Desktop/JosephTawilProjects-master/TankWarGame/src/Tank/Objects/Movable/Movable.java
@@ -0,0 +1,19 @@
+package Tank.Objects.Movable;
+import Tank.Objects.GameObject;
+import java.awt.image.BufferedImage;
+
+public class Movable extends GameObject
+{
+    protected int speed;
+
+    public Movable()
+    {
+
+    }
+    public Movable(BufferedImage img, int x, int y, int speed)
+    {
+        super(x,y,img,null);
+        this.speed = speed;
+
+    }
+}

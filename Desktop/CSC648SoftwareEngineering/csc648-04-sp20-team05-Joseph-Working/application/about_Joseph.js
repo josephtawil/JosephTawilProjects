363cd5824940589a6ad2aa2d220b99f2a7eee7c0
Joseph Tawil <jtawil@mail.sfsu.edu>
2020-02-27 19:02:07 -0800

    updating about file

---
+++ Desktop/CSC648SoftwareEngineering/csc648-04-sp20-team05-Joseph-Working/application/about_Joseph.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<!-- saved from url=(0051)file:///home/joseph/Desktop/about%20page/index.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body>
+        <h1 style="text-align: center"> About Page Joseph Tawil</h1>
+        <img src="./about_Joseph_files/mypicture.jpeg" width="300px" height="300px">
+        <p style="text-align: center">
+            Hi my name is Joseph Tawil and I am a upcoming software engineer at San Francisco State University. I like to code and play basketball.
+        </p>
+    
+</body></html>
